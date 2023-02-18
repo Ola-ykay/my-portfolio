6b3e43d3e4c2b193cf2155764946f657dd4fb481
ykay11 <>
2023-02-18 18:30:16 +0000

    first-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './App.css'
+import './App.css';
 import Navbar from './Components/Navbar'
 import Intro from './Components/Intro'
 import About from './Components/About'
