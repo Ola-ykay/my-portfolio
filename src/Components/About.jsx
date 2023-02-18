dfebb146ec0830bbf1d0e8dbb643109e7a6bf6a3
Olayinka Fagbemi <fagbemio123@gmail.com>
2023-02-18 17:41:00 +0000

    First commit

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -28,10 +28,10 @@ import '../Styles/About.css';
         ABOUT ME
       </motion.h2>
     </div>
-      <motion.p  initial={{opacity:0, x:100}}
+      <motion.p initial={{opacity:0, x:100}}
           whileInView={{opacity : 1 , x:0}}
           viewport={{once : false}}
-          transition = {{ duration:2, bounce: 3,stiffness:80,
+          transition = {{ duration:2, bounce: 3, stiffness:80,
               ease:"easeInOut"}}>
         I am Olayinka Fagbemi, currently based in Nigeria. I transitioned from viewing micro-organisms on a culture plate to writing lines of codes on a web page.   
        I am hardworking, innovative, goal driven and have keen interest in frontend engineering as I love to create accessible applications with beautiful and delightful user experiences, solve problems and share knowledge. I aim to make the world a better place with my skillset.
